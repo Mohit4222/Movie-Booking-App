213ef6278fdf5ab898284c89c2a9c44c341bef94
Mohit <mohitsharma4222@gmail.com>
2022-04-15 12:21:21 +0530

    Checkpoint 2

---
+++ movies-app/src/Common/header/header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './Header.css';
+
+
+export default class Header extends Component {
+  render() {
+    return (
+      <>
+      <div className='header'>
+
+      <img src=".Assets/logo.svg" className='logo' />
+        
+      </div>
+      </>
+    )
+  }
+}
---
+++ movies-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Common/header/header';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+     <Header></Header>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
